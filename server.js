9c1a91ff8885723dc86dd7978187eef2d7db8dd6
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-05-08 16:54:48 +0530

    fix task model

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const projectRoutes = require("./routes/projectRoutes");
 const app = express();
 
 
+
 // Middleware
 app.use(cors());
+
 app.use(express.json());
+
+
 
 
 // Routes
@@ -24,35 +28,29 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/projects", projectRoutes);
 
 
+
+
+// Connect Database
+connectDB();
+
+
+
+
 // Test Route
 app.get("/", (req, res) => {
+
   res.send("API Running");
+
 });
 
 
+
+
 // Start Server
-const startServer = async () => {
+const PORT = process.env.PORT || 5000;
 
-  try {
+app.listen(PORT, () => {
 
-    console.log("Connecting to MongoDB...");
+  console.log(`Server running on port ${PORT}`);
 
-    await connectDB();
-
-    console.log("MongoDB Connected");
-
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-
-  } catch (error) {
-
-    console.log(error);
-
-  }
-
-};
-
-startServer();
+});
